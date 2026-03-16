7f319c72c4590f39d08b38ee47719dc12a1dfc40
prayudahlah <yudafihan@gmail.com>
2026-03-16 22:13:45 +0700

    refactor: move products schema to where it's supposed to be

--- backend/src/schemas/products.py
+++ backend/src/schemas/products.py
@@ -1,1 +1,17 @@
-from src.schemas.products import products
+from pydantic import BaseModel
+
+
+class Product(BaseModel):
+    id: int
+    name: str
+    price: float
+    stock: int
+
+
+products = [
+    Product(id=1, name="Beras", price=35000, stock=15),
+    Product(id=2, name="Kopi", price=15000, stock=20),
+    Product(id=3, name="Minyak", price=45000, stock=25),
+    Product(id=4, name="Tepung", price=8000, stock=50),
+    Product(id=5, name="Sabun", price=5000, stock=30),
+]
